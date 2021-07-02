c9dec4b8596df8edf6d6a48999f74e51e370e06d
Utkarsh Raj Singh <70368997+UtkarshRajSingh@users.noreply.github.com>
2021-07-02 13:48:48 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,28 @@
+import turtle
+a=5
+print(a)
+b="hello"
+print(b)
+
+credit_card = 4444333322221111
+
+print(credit_card)
+
+
+# comment this is a square
+utkarsh_turtle=turtle.Turtle()
+def square():
+    utkarsh_turtle.forward(100)
+    utkarsh_turtle.right(90)
+    utkarsh_turtle.forward(100)
+    utkarsh_turtle.right(90)
+    utkarsh_turtle.forward(100)
+    utkarsh_turtle.right(90)
+    utkarsh_turtle.forward(100)
+    utkarsh_turtle.right(90)
+    utkarsh_turtle.forward(100)
+  
+square()
+utkarsh_turtle.forward(200)
+square()
+
